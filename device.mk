ef4aa705b46db27b72f3d843fa59a0b6cdf59e24
123 <no@thanks.com>
2026-07-15 13:35:26 +0800

    sensors: switch mock -> multihal to fix desktop blocker (ISensors/default never registers)
    mock service rc lacks 'interface' line and the service never starts via
    class hal; system_server blocks 65s in SystemSensorManager.nativeCreate
    -> watchdog kill -> no desktop. Align with Lenovo zippo (sm8150-common):
    use android.hardware.sensors@2.0-service.multihal + ScopedWakelock.vendor.
    multihal module provides rc (class hal) + vintf fragment; vendor already
    ships SSC sub-HAL (sensors.ssc.so, sensors.qti, hals.conf, rcs).
    
    Refs: ref/zippo/android_device_lenovo_zippo/device.mk:29-31

--- device.mk
+++ device.mk
@@ -501,7 +501,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.0-service.mock \
+    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
     libsensorndkbridge
 
 # Servicetracker
