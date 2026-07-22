5178779764ed5b47f210e1d04e3781d1645dc537
123 <no@thanks.com>
2026-07-22 23:35:58 +0900

    device: drop AIDL USB HAL, restore A13 prebuilt approach

--- device.mk
+++ device.mk
@@ -81,8 +81,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    vendor/qcom/opensource/usb
+    $(LOCAL_PATH)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1200
@@ -265,8 +264,6 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti \
-    android.hardware.usb.gadget-service.qti
 
 
 PRODUCT_COPY_FILES += \
