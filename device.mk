728026496cad649e3c07a7f54428ace9cf8235f6
123 <no@thanks.com>
2026-07-23 01:20:47 +0900

    fixup: restore USB Soong namespace to etc/ only
    USB HAL modules (hal/Android.bp) are found via default vendor paths,
    no namespace needed. Only etc/ needs namespace for the init scripts.

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    vendor/qcom/opensource/usb
+    vendor/qcom/opensource/usb/etc
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1200
