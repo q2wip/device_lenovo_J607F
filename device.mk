87de0c000c99a4f49339907db64ba37f58f34b49
123 <no@thanks.com>
2026-07-26 18:47:22 +0800

    BT: remove android.hardware.bluetooth.audio-impl + bluetooth_audio.xml VINTF fragment
    The AIDL IBluetoothAudioProviderFactory service has no service binary
    to register it - only the shared library exists. The VINTF fragment
    declares it as available, but servicemanager can't start it (no .rc),
    causing the BT stack to loop retrying.
    
    FP4 reference does not use this AIDL VINTF fragment; it relies on
    the HIDL vendor.qti.hardware.bluetooth_audio@2.1 instead.
    
    Removed:
    - device_lenovo_J607F/device.mk: android.hardware.bluetooth.audio-impl
    - device_lenovo_J607F/proprietary-files.txt: bluetooth_audio.xml,
      android.hardware.bluetooth.audio-impl.so (32+64)
    - vendor_lenovo_J607Z/proprietary-files.txt: audio-impl.so (32+64)
    - vendor_lenovo_J607Z/J607Z-vendor.mk: audio-impl PRODUCT_COPY_FILES (32+64)

--- device.mk
+++ device.mk
@@ -249,7 +249,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     liba2dpoffload \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
