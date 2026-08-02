d29a819e7c5acb36f55215a16c6553389d395fdb
123 <no@thanks.com>
2026-08-02 21:39:59 +0800

    Revert "device: declare android.hardware.microphone feature permission"
    The file frameworks/native/data/etc/android.hardware.microphone.xml does
    not exist in A16 (removed from AOSP); the feature is already declared via
    tablet_core_hardware.xml, which the framework package installs implicitly
    (pm list features shows android.hardware.microphone without any vendor
    permission file).

--- device.mk
+++ device.mk
@@ -100,8 +100,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Audio
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
-    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
-    frameworks/native/data/etc/android.hardware.microphone.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.microphone.xml
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
